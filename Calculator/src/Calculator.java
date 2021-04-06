7f6516485f6a721d5a9414fbdfcfc48d004470c3
Sai Nitesh <67225871+sainiteshb@users.noreply.github.com>
2021-04-06 17:26:09 +0530

    Added panel

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -60,6 +60,27 @@ public class Calculator implements ActionListener {
 			numberButtons[i].setFocusable(false);
 		}
 
+		panel = new JPanel();
+		panel.setBounds(50, 100, 300, 300);
+		panel.setLayout(new GridLayout(4,4,10,10));
+
+		panel.add(numberButtons[1]);
+		panel.add(numberButtons[2]);
+		panel.add(numberButtons[3]);
+		panel.add(addButton);
+		panel.add(numberButtons[4]);
+		panel.add(numberButtons[5]);
+		panel.add(numberButtons[6]);
+		panel.add(subButton);
+		panel.add(numberButtons[7]);
+		panel.add(numberButtons[8]);
+		panel.add(numberButtons[9]);
+		panel.add(mulButton);
+		panel.add(decButton);
+		panel.add(numberButtons[0]);
+		panel.add(equButton);
+		panel.add(divButton);
+		
 	        frame.add(panel);
 		frame.add(negButton);
 		frame.add(delButton);
